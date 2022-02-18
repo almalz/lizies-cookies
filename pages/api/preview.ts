c12f4f778f5e6f5c81ee73487a6ae837b9cd2600
Alexis Malzieu <alex.malzieu@gmail.com>
2022-02-18 14:52:50 +0100

    🔥 hide preview page

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,39 +1,40 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import client from '../../lib/apolloClient'
-import {
-  DropDatesDocument,
-  DropDatesQuery,
-} from '../../types/generated/graphql'
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import client from '../../lib/apolloClient'
+// import {
+//   DropDatesDocument,
+//   DropDatesQuery,
+// } from '../../types/generated/graphql'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  console.log(req.query)
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   // Check the secret and next parameters
+//   // This secret should only be known to this API route and the CMS
+//   console.log(req.query)
 
-  if (req.query.secret !== 'DATOCMS_PREVIEW_SECRET' || !req.query.slug) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
+//   if (req.query.secret !== 'DATOCMS_PREVIEW_SECRET' || !req.query.slug) {
+//     return res.status(401).json({ message: 'Invalid token' })
+//   }
 
-  const { data } = await client.query<DropDatesQuery>({
-    query: DropDatesDocument,
-  })
+//   const { data } = await client.query<DropDatesQuery>({
+//     query: DropDatesDocument,
+//   })
 
-  // Get the paths we want to pre-render based on posts
-  const paths = data.allDrops.map((drop) => ({
-    params: { id: drop.deliveryDate },
-  }))
+//   // Get the paths we want to pre-render based on posts
+//   const paths = data.allDrops.map((drop) => ({
+//     params: { id: drop.deliveryDate },
+//   }))
 
-  // If the slug doesn't exist prevent preview mode from being enabled
-  if (!data) {
-    return res.status(401).json({ message: 'Invalid slug' })
-  }
+//   // If the slug doesn't exist prevent preview mode from being enabled
+//   if (!data) {
+//     return res.status(401).json({ message: 'Invalid slug' })
+//   }
 
-  // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
+//   // Enable Preview Mode by setting the cookies
+//   res.setPreviewData({})
 
-  // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.redirect(post.slug)
-}
+//   // Redirect to the path from the fetched post
+//   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
+//   res.redirect(post.slug)
+// }
 
-export default handler
+// export default handler
+export {}
