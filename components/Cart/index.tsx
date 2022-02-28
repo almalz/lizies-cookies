234cda96b37f163af6d58e486af5e44439beb59b
Alexis Malzieu <alex.malzieu@gmail.com>
2022-02-28 15:40:58 +0100

    🎨  put back snipcart sdk item count on cart

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -1,37 +1,35 @@
 import { Circle, Spinner } from '@chakra-ui/react'
 import Image from 'next/image'
-import { useCallback } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import CartIcon from '../../assets/icons/cart.svg'
 import { useSnipcart } from '../../lib/snipcart'
 
 const Cart: React.FC = () => {
   const { Snipcart, loading } = useSnipcart()
 
+  const [itemCount, setItemCount] = useState<number | null>(null)
+
   const handleClick = useCallback(() => {
     Snipcart?.cart?.open()
   }, [Snipcart?.cart])
+
+  //value subscription when cart changes
+  useEffect(() => {
+    let unsubscribe: () => void
+    unsubscribe = Snipcart?.store?.subscribe(async () => {
+      const _itemCount = await Snipcart?.store?.itemCount()
+      setItemCount(_itemCount || null)
+    })
+    return () => {
+      unsubscribe && unsubscribe()
+    }
+  }, [Snipcart?.store])
 
   return (
     <button onClick={handleClick}>
       <Circle p="4px" pos="relative">
         <Image src={CartIcon} width={42} height={42} alt="cart_icon" />
-        {loading && (
-          <Circle
-            pos="absolute"
-            top="-2"
-            right="-2"
-            size="28px"
-            zIndex={3}
-            bg="gray.900"
-            color="white"
-            fontWeight={700}
-            fontSize="md"
-            border="2px solid"
-            borderColor="white"
-          >
-            <Spinner color="White" size="xs" />
-          </Circle>
-        )}
+
         <Circle
           pos="absolute"
           top="-2"
@@ -45,7 +43,11 @@ const Cart: React.FC = () => {
           border="2px solid"
           borderColor="white"
         >
-          <span className="snipcart-items-count"></span>
+          {loading ? (
+            <Spinner color="White" size="xs" />
+          ) : (
+            <span>{itemCount || ''}</span>
+          )}
         </Circle>
       </Circle>
     </button>
