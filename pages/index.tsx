3f885a3d42d3fa18ab760b3ffd2885ffef2be3fc
Alexis Malzieu <alex.malzieu@gmail.com>
2022-03-01 15:53:46 +0100

    ✨ WIP state

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,96 +33,9 @@ export type DropPageProps = {
 }
 
 const Home: NextPage<DropPageProps> = ({ drop, pageBody }) => {
-  const router = useRouter()
-
-  // while on the page, check every 5 minutes, if a drop is still currently scheluded
-  // if not, redirects to /nodrop page
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      const { data } = await client.query<NextIncomingDropsQuery>({
-        query: NextIncomingDropsDocument,
-        variables: { TODAY: TODAY },
-        fetchPolicy: 'no-cache',
-      })
-      const _drop = data.allDrops[0]
-      if (!_drop) {
-        router.push('/nodrop')
-      } else if (_drop.id !== drop.id) {
-        router.reload()
-      }
-    }, REFRESH_INTERVAL)
-    return () => clearInterval(interval)
-  }, [router, drop.id])
-
   return (
     <Layout seo={pageBody.seo || undefined} noIndex={pageBody.noindex} slug="">
-      <Flex h={['100vh', '100vh', '100vh', '100vh']}>
-        <RBox desktopOnly w="40%" h="100%">
-          <Flex
-            h="100%"
-            flexDir={'column'}
-            boxShadow="inner"
-            px={['0', '0', '64px', '64px']}
-            alignItems="center"
-            justifyContent="center"
-          >
-            {pageBody && <DropSummary drop={drop} pageBody={pageBody} />}
-            <Box mt="10%" w={['0', '0', '200px', '300px', '400px']} mx="auto">
-              <Links />
-            </Box>
-          </Flex>
-        </RBox>
-        <Box
-          pos="relative"
-          w={['100%', '100%', '100%', '60%']}
-          h="100%"
-          boxShadow="2xl"
-          overflowY="scroll"
-          maxH={['auto', 'auto', 'auto', '100%']}
-        >
-          <RBox
-            mobileOnly
-            pos="relative"
-            px={['16px', '16px', '96px', '0px']}
-            pt={['16px', '16px', '32px', '0px']}
-          >
-            <RFlex
-              mobileOnly
-              pos="sticky"
-              justifyContent={'end'}
-              pr={['16px', '16px', '96px', '0px']}
-              pt={['16px', '16px', '32px', '0px']}
-            >
-              <Cart />
-            </RFlex>
-            {pageBody && <DropSummary drop={drop} pageBody={pageBody} />}
-          </RBox>
-
-          <Flex
-            justifyContent="center"
-            alignItems={['start', 'start', 'start', 'start']}
-            py={['0', '0', '0', '10vh']}
-          >
-            <RFlex
-              px={['16px', '16px', '96px', '96px']}
-              pt={['32px', '32px', '2%', '2%']}
-              flexDir={'column'}
-              h="100%"
-            >
-              <RFlex desktopOnly justifyContent="end" py="8px">
-                <Cart />
-              </RFlex>
-              <Box pb={['32px', '64px', '96px', '96px']}>
-                <ProductList products={drop.products} />
-              </Box>
-              <RBox mobileOnly px="32px" pb="32px">
-                <Links />
-              </RBox>
-            </RFlex>
-          </Flex>
-        </Box>
-        <ThresholdModal />
-      </Flex>
+      Site en construction
     </Layout>
   )
 }
