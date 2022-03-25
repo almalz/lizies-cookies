3ba68e2ca0c321c702b24496ad548291ad8acb79
Alexis Malzieu <alex.malzieu@gmail.com>
2022-03-25 18:32:47 +0100

    💄 improve links display

--- components/Links/index.tsx
+++ components/Links/index.tsx
@@ -1,43 +1,61 @@
-import { Flex, Link } from '@chakra-ui/react'
+import { Flex, Link as NextLink } from '@chakra-ui/react'
 
-export type Link = {
+export type TLink = {
   label: string
   href: string
-  isExtrenal: boolean
+  isExternal: boolean
+  sx?: any
 }
 
-const LINKS: Link[] = [
-  { label: 'FAQ', href: '/faq', isExtrenal: false },
-  { label: 'Mentions légales', href: '/legal', isExtrenal: false },
+const LINKS: TLink[] = [
+  { label: 'Mentions légales', href: '/legal', isExternal: false },
+  { label: 'CGV', href: '/terms', isExternal: false },
   {
     label: 'Politique de confidentialité',
     href: '/privacy',
-    isExtrenal: false,
+    isExternal: false,
   },
-  { label: 'CGV', href: '/terms', isExtrenal: false },
+  {
+    label: 'Foire aux questions',
+    href: '/faq',
+    isExternal: false,
+    sx: { flex: '0 2 50%', textAlign: 'right' },
+  },
   {
     label: 'Contact',
     href: 'mailto:hello@liziescookies.fr',
-    isExtrenal: true,
+    isExternal: true,
+    sx: { flex: '0 2 50%', textAlign: 'left' },
   },
 ]
 
+const Link: React.FC<{ link: TLink }> = ({ link }) => {
+  return (
+    <NextLink
+      key={`${link.href}-${link.label}`}
+      href={link.href}
+      isExternal={link.isExternal}
+      fontWeight={600}
+      fontSize="xs"
+      color="gray.600"
+      mx="8px"
+      sx={link.sx}
+    >
+      {link.label}
+    </NextLink>
+  )
+}
+
 const Links: React.FC = () => {
   return (
-    <Flex justify="space-around" flexWrap="wrap">
-      {LINKS.map((link: Link) => (
-        <Link
-          key={`${link.href}-${link.label}`}
-          href={link.href}
-          isExternal={link.isExtrenal}
-          fontWeight={600}
-          fontSize="sm"
-          color="gray.600"
-          mx="4px"
-        >
-          {link.label}
-        </Link>
-      ))}
+    <Flex w="100%" justify="center" flexWrap="wrap">
+      <Link link={LINKS[0]} />
+      <Link link={LINKS[1]} />
+      <Link link={LINKS[2]} />
+      <Flex w="100%" justify="space-around">
+        <Link link={LINKS[3]} />
+        <Link link={LINKS[4]} />
+      </Flex>
     </Flex>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -59,18 +59,16 @@ const Home: NextPage<DropPageProps> = ({ drop, pageBody }) => {
   return (
     <Layout seo={pageBody.seo || undefined} noIndex={pageBody.noindex} slug="">
       <Flex h={['100%', '100%', '100%', '100vh']}>
-        <RBox desktopOnly w="40%" h="100%">
+        <RBox desktopOnly w="40%" h="100%" overflowY="auto">
           <Flex
             h="100%"
             flexDir={'column'}
             boxShadow="inner"
             px={['0', '0', '64px', '64px']}
-            alignItems="center"
-            justifyContent="center"
-            py={['0', '0', '0', '10vh']}
+            pt={['0', '0', '0', '10vh']}
           >
             {pageBody && <DropSummary drop={drop} pageBody={pageBody} />}
-            <Box mt="10%" w={'100%'}>
+            <Box mt="auto" pb="32px" w={'100%'}>
               <Links />
             </Box>
           </Flex>
@@ -80,7 +78,7 @@ const Home: NextPage<DropPageProps> = ({ drop, pageBody }) => {
           w={['100%', '100%', '100%', '60%']}
           h="100%"
           boxShadow="2xl"
-          overflowY="scroll"
+          overflowY="auto"
           maxH={['auto', 'auto', 'auto', '100%']}
         >
           <RBox
@@ -104,7 +102,7 @@ const Home: NextPage<DropPageProps> = ({ drop, pageBody }) => {
           <Flex
             justifyContent="center"
             alignItems={['start', 'start', 'start', 'start']}
-            pt={['0px', '0px', '0px', '5vh', '5vh']}
+            pt={['0px', '0px', '0px', '5vh', '10vh']}
             h="100%"
           >
             <RFlex
