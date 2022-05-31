eb9283af3278ec67f787e29257da67280b9c2e66
Alexis Malzieu <alex.malzieu@gmail.com>
2022-05-31 10:41:08 +0200

    🔥  hide recurrent drop fetching

--- lib/store/products/provider.tsx
+++ lib/store/products/provider.tsx
@@ -27,27 +27,27 @@ const ProductsProvider: React.FC = ({ children }) => {
     string | null | undefined
   >()
 
-  // fetch the next drop every REFRESH_INTERVAL
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      const drop = await Products.getCurrentDrop()
-      if (!drop) {
-        if (currentDropId) {
-          currentDropId !== undefined &&
-            console.info('Drop update : no current drop anymore ')
-          setCurrentDropId(null)
-        }
-        return
-      }
+  // // fetch the next drop every REFRESH_INTERVAL
+  // useEffect(() => {
+  //   const interval = setInterval(async () => {
+  //     const drop = await Products.getCurrentDrop()
+  //     if (!drop) {
+  //       if (currentDropId) {
+  //         currentDropId !== undefined &&
+  //           console.info('Drop update : no current drop anymore ')
+  //         setCurrentDropId(null)
+  //       }
+  //       return
+  //     }
 
-      if (drop.id === currentDropId) return
+  //     if (drop.id === currentDropId) return
 
-      setCurrentDropId(drop.id)
-      currentDropId !== undefined &&
-        console.info('Drop update : another drop has started')
-    }, REFRESH_INTERVAL)
-    return () => clearInterval(interval)
-  }, [currentDropId])
+  //     setCurrentDropId(drop.id)
+  //     currentDropId !== undefined &&
+  //       console.info('Drop update : another drop has started')
+  //   }, REFRESH_INTERVAL)
+  //   return () => clearInterval(interval)
+  // }, [currentDropId])
 
   const value = { loading, setLoading, Products, currentDropId }
   return (
