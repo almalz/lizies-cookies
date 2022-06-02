8feed8ecf733d515ef72de437c5c5396e9f8c06a
Alexis Malzieu <alex.malzieu@gmail.com>
2022-06-02 23:37:12 +0200

    🎨 contact better error handeling

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -64,83 +64,126 @@ const ContactPage: NextPage<ContactPageProps> = ({ contactpage }) => {
           <ParagraphXl>{contactpage.header}</ParagraphXl>
         </div>
         <div className="py-4 md:px-8 lg:px-[20%]">
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-2 pb-8 font-body text-pink-500"
+          >
             <FormControl
-              className="flex flex-col gap-2 pb-8 font-body text-pink-500"
-              isInvalid={Object.keys(errors).length > 0}
+              className="flex flex-col gap-1"
+              isInvalid={!!errors.firstName}
             >
-              <div className="flex flex-col gap-1">
-                <FormLabel htmlFor="firstname" m="0" fontWeight="400">
-                  {contactpage.firstnameLabel}*
-                </FormLabel>
-                <Input
-                  id="firstname"
-                  {...register('firstName', {
-                    required: 'Ce champ est requis',
-                  })}
-                  rounded="none"
-                  borderColor="#F3A1A2"
-                  border="2px solid"
-                  focusBorderColor="#f8c7c7"
-                  _hover={{ borderColor: '#F3A1A2' }}
-                  _focus={{
-                    background: '#F3A1A211',
-                  }}
-                />
-                <FormErrorMessage>
-                  {errors.firstName && errors.firstName.message}
-                </FormErrorMessage>
-              </div>
-              <div className="flex flex-col gap-1">
-                <FormLabel htmlFor="name" m="0" fontWeight="400">
-                  {contactpage.nameLabel}*
-                </FormLabel>
-                <Input
-                  id="name"
-                  {...register('lastName', {
-                    required: 'Ce champ est requis',
-                  })}
-                  rounded="none"
-                  borderColor="#F3A1A2"
-                  border="2px solid"
-                  focusBorderColor="#f8c7c7"
-                  _hover={{ borderColor: '#F3A1A2' }}
-                  _focus={{
-                    background: '#F3A1A211',
-                  }}
-                />
-                <FormErrorMessage>
-                  {errors.lastName && errors.lastName.message}
-                </FormErrorMessage>
-              </div>
-              <div className="flex flex-col gap-1">
-                <FormLabel htmlFor="email" m="0" fontWeight="400">
-                  {contactpage.emailLabel}*
-                </FormLabel>
-                <Input
-                  id="email"
-                  {...register('email', {
-                    required: 'Ce champ est requis',
-                  })}
-                  rounded="none"
-                  borderColor="#F3A1A2"
-                  border="2px solid"
-                  focusBorderColor="#f8c7c7"
-                  _hover={{ borderColor: '#F3A1A2' }}
-                  _focus={{
-                    background: '#F3A1A211',
-                  }}
-                />
-                <FormErrorMessage>
-                  {errors.email && errors.email.message}
-                </FormErrorMessage>
-              </div>
-              <div className="flex flex-col gap-1">
-                <FormLabel htmlFor="phone" m="0" fontWeight="400">
-                  {contactpage.phoneLabel}*
-                </FormLabel>
-                <Input
-                  id="phone"
+              <FormLabel htmlFor="firstname" m="0" fontWeight="400">
+                {contactpage.firstnameLabel}*
+              </FormLabel>
+              <Input
+                id="firstname"
+                {...register('firstName', {
+                  required: 'Ce champ est requis',
+                })}
+                rounded="none"
+                borderColor="#F3A1A2"
+                border="2px solid"
+                focusBorderColor="#f8c7c7"
+                _hover={{ borderColor: '#F3A1A2' }}
+                _focus={{
+                  background: '#F3A1A211',
+                }}
+              />
+              <FormErrorMessage>
+                {errors.firstName && errors.firstName.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <FormControl
+              className="flex flex-col gap-1"
+              isInvalid={!!errors.lastName}
+            >
+              <FormLabel htmlFor="name" m="0" fontWeight="400">
+                {contactpage.nameLabel}*
+              </FormLabel>
+              <Input
+                id="name"
+                {...register('lastName', {
+                  required: 'Ce champ est requis',
+                })}
+                rounded="none"
+                borderColor="#F3A1A2"
+                border="2px solid"
+                focusBorderColor="#f8c7c7"
+                _hover={{ borderColor: '#F3A1A2' }}
+                _focus={{
+                  background: '#F3A1A211',
+                }}
+              />
+              <FormErrorMessage>
+                {errors.lastName && errors.lastName.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              className="flex flex-col gap-1"
+              isInvalid={!!errors.email}
+            >
+              <FormLabel htmlFor="email" m="0" fontWeight="400">
+                {contactpage.emailLabel}*
+              </FormLabel>
+              <Input
+                id="email"
+                {...register('email', {
+                  required: 'Ce champ est requis',
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: 'Entered value does not match email format',
+                  },
+                })}
+                type="email"
+                rounded="none"
+                borderColor="#F3A1A2"
+                border="2px solid"
+                focusBorderColor="#f8c7c7"
+                _hover={{ borderColor: '#F3A1A2' }}
+                _focus={{
+                  background: '#F3A1A211',
+                }}
+              />
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              className="flex flex-col gap-1"
+              isInvalid={!!errors.phone}
+            >
+              <FormLabel htmlFor="phone" m="0" fontWeight="400">
+                {contactpage.phoneLabel}*
+              </FormLabel>
+              <Input
+                id="phone"
+                {...register('phone', {
+                  required: 'Ce champ est requis',
+                })}
+                rounded="none"
+                borderColor="#F3A1A2"
+                border="2px solid"
+                focusBorderColor="#f8c7c7"
+                _hover={{ borderColor: '#F3A1A2' }}
+                _focus={{
+                  background: '#F3A1A211',
+                }}
+              />
+              <FormErrorMessage>
+                {errors.phone && errors.phone.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              className="flex flex-col gap-1"
+              isInvalid={!!errors.object}
+            >
+              <FormLabel htmlFor="object" m="0" fontWeight="400">
+                {contactpage.objectLabel}*
+              </FormLabel>
+              {contactpage?.objects && (
+                <Select
+                  id="object"
                   {...register('phone', {
                     required: 'Ce champ est requis',
                   })}
@@ -152,67 +195,46 @@ const ContactPage: NextPage<ContactPageProps> = ({ contactpage }) => {
                   _focus={{
                     background: '#F3A1A211',
                   }}
-                />
-                <FormErrorMessage>
-                  {errors.phone && errors.phone.message}
-                </FormErrorMessage>
-              </div>
-              <div className="flex flex-col gap-1">
-                <FormLabel htmlFor="object" m="0" fontWeight="400">
-                  {contactpage.objectLabel}*
-                </FormLabel>
-                {contactpage?.objects && (
-                  <Select
-                    id="object"
-                    {...register('phone', {
-                      required: 'Ce champ est requis',
-                    })}
-                    rounded="none"
-                    borderColor="#F3A1A2"
-                    border="2px solid"
-                    focusBorderColor="#f8c7c7"
-                    _hover={{ borderColor: '#F3A1A2' }}
-                    _focus={{
-                      background: '#F3A1A211',
-                    }}
-                  >
-                    {extractObject(contactpage?.objects).map((object) => {
-                      return (
-                        <option key={object} value={object}>
-                          {object}
-                        </option>
-                      )
-                    })}
-                  </Select>
-                )}
-                <FormErrorMessage>
-                  {errors.phone && errors.phone.message}
-                </FormErrorMessage>
-              </div>
-              <div className="flex flex-col gap-1">
-                <FormLabel htmlFor="message" m="0" fontWeight="400">
-                  {contactpage.messageLabel}*
-                </FormLabel>
-                <Textarea
-                  id="message"
-                  {...register('message', {
-                    required: 'Ce champ est requis',
+                >
+                  {extractObject(contactpage?.objects).map((object) => {
+                    return (
+                      <option key={object} value={object}>
+                        {object}
+                      </option>
+                    )
                   })}
-                  resize="none"
-                  rounded="none"
-                  borderColor="#F3A1A2"
-                  border="2px solid"
-                  focusBorderColor="#f8c7c7"
-                  _hover={{ borderColor: '#F3A1A2' }}
-                  _focus={{
-                    background: '#F3A1A211',
-                  }}
-                  height="10rem"
-                />
-                <FormErrorMessage>
-                  {errors.message && errors.message.message}
-                </FormErrorMessage>
-              </div>
+                </Select>
+              )}
+              <FormErrorMessage>
+                {errors.phone && errors.phone.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              className="flex flex-col gap-1"
+              isInvalid={!!errors.message}
+            >
+              <FormLabel htmlFor="message" m="0" fontWeight="400">
+                {contactpage.messageLabel}*
+              </FormLabel>
+              <Textarea
+                id="message"
+                {...register('message', {
+                  required: 'Ce champ est requis',
+                })}
+                resize="none"
+                rounded="none"
+                borderColor="#F3A1A2"
+                border="2px solid"
+                focusBorderColor="#f8c7c7"
+                _hover={{ borderColor: '#F3A1A2' }}
+                _focus={{
+                  background: '#F3A1A211',
+                }}
+                height="10rem"
+              />
+              <FormErrorMessage>
+                {errors.message && errors.message.message}
+              </FormErrorMessage>
             </FormControl>
             <div className="flex justify-center">
               <Button loading={isSubmitting} type="submit">
