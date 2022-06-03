228e2d5e8a14fc0f5f3a82a3ec155f8d0d627e7e
Alexis Malzieu <alex.malzieu@gmail.com>
2022-06-03 18:59:13 +0200

    🤡 change dummy social links
    (cherry picked from commit 090c81208824578dddc519b02dd51ab07b9e1bfe)

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -19,21 +19,21 @@ export const Footer: React.FC = () => {
     <footer className="flex w-full flex-col items-center gap-y-8 bg-pink-gray py-6">
       <ul className="flex items-center gap-x-8 px-2 py-2">
         <li>
-          <Link href="https://www.instagram.com/_naughtycookies/">
+          <Link href="https://www.instagram.com/naughtycookies_/">
             <a target="_blank">
               <FaInstagram className="text-white hover:text-purple-200" />
             </a>
           </Link>
         </li>
         <li>
-          <Link href="https://www.tiktok.com/@naughtycookies">
+          <Link href="http://www.tiktok.com/@naughtycookies_">
             <a target="_blank">
               <FaTiktok className="text-white hover:text-purple-200" />
             </a>
           </Link>
         </li>
         <li>
-          <Link href="https://www.facebook.com/naughtycookies">
+          <Link href="https://www.facebook.com/ngtycookies">
             <a target="_blank">
               <FaFacebook className="text-white hover:text-purple-200" />
             </a>
