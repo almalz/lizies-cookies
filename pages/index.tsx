9e011b3e7887d647bce52c422c242b50b631796d
Alexis Malzieu <alex.malzieu@gmail.com>
2022-06-03 18:37:38 +0200

    ✏️

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <Layout seo={undefined} slug="" className="flex h-screen flex-col">
       <div className="flex flex-1 flex-col items-center justify-center gap-y-12">
         <h1 className="font-body text-3xl font-bold text-purple-700 sm:text-6xl">
-          ouverture de la boutique
+          ouverture des commandes
         </h1>
         <span className="font-body text-3xl font-bold text-pink-500 sm:text-5xl">
           le jeudi 16 juin 2022
