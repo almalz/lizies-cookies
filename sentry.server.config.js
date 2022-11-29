1a0722b1899944d9128bce53b1dc4fbe229e0f97
Alexis Malzieu <alex.malzieu@gmail.com>
2022-11-29 22:39:27 +0100

    ✏️

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
-const SENTRY_ENV = process.env.NEXT_PUBLIC_ENV || 'developement'
+const SENTRY_ENV = process.env.NEXT_PUBLIC_ENV || 'development'
 
 Sentry.init({
   environment: SENTRY_ENV,
